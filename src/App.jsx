f137bb2a5e4c70814718e9687dc3828605857dee
Meghann Hlibichuk <meghannfh@gmail.com>
2023-03-07 00:16:56 -0600

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import Button from './components/Button';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [darkMode, setDarkMode] = useState(false)
+
+  const John = {
+    age: 1,
+    position: 'Coffee Maker',
+  }
+  const Jane = {
+    age: 2,
+    position: 'Bread Maker'
+  }
+
+  const workers = [John, Jane];
+
+  const data = [
+    'asdfasf',
+    'asfsdfs',
+    'asfsfsdf',
+    'asfasfsa',
+    'asfasf'
+  ]
+  
+  const x = 0;
+  const handleClick = () => {
+    setCount((count) => count + 1)
+    setDarkMode((darkMode) => !darkMode)
+  }
+
+  darkMode ? document.body.style.backgroundColor = '' : document.body.style.backgroundColor = 'white';
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <div className="">
+    <div className="card">
+      <button onClick={handleClick}>
+        count is {count}
+      </button>
+      {workers.map((el,i) => {
+        return <Button key={i} text={el.position} />
+      })
+    }
     </div>
+  </div>
   )
 }
 
