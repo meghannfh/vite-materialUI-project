6c3fffa7d3137db0556307d41141805a633c21f6
Minh Doan <minhadoan@gmail.com>
2023-04-08 16:48:10 -0500

    Deleted unused pages

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,12 +4,10 @@ export default function Footer() {
     return (
         <>
             <ul>
-                <Link to='/profile'>Profile</Link>
-                <Link to='/about'>About</Link>
-                <Link to='/addCard'>Add a card</Link>
                 <Link to='/'>Home</Link>
                 <Link to='/login'>Login</Link>
-                <Link to='/signup'>Signup</Link>
+                <Link to='/profile'>Profile</Link>
+                <Link to='/about'>About</Link>
             </ul>
         </>
     )
